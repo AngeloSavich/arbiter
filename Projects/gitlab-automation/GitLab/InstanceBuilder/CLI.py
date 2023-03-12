96a7ec56123e07bc23f5005d204661000b99eaf1
Angelo Savich <AngeloSavich@gmail.com>
2023-03-12 18:39:52 -0400

    100 - Adds gitlab-automation project

---
+++ Projects/gitlab-automation/GitLab/InstanceBuilder/CLI.py
@@ -0,0 +1,19 @@
+
+
+# Cli Command that reads the gitlab url, key, and scope (group or project) out of a config and builds a .resource class file that has the storngly typed properties so you can do gitlabinstance.fanuc.issues-ngc
+
+# You can then do one for projects (issues, merge requests, scripts, labels), groups (labels, etc),syncing across multiple groups and projects (issues and merge requests and branches)
+import gitlab
+import Config
+
+gl = gitlab.Gitlab(url=Config.url_gitlab, private_token=Config.gitlab_access_token)
+
+if Config.Runtime.Mode:
+    gl.auth()
+    gl.enable_debug()
+
+def Start():
+    # list all the projects
+    projects = gl.projects.list(iterator=True)
+    for project in projects:
+        print(project)
