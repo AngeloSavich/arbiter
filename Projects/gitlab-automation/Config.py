3292747c427f4632462fc85dedc4152e788116be
Angelo Savich <AngeloSavich@gmail.com>
2023-03-12 18:39:52 -0400

    100 - Adds gitlab-automation project

---
+++ Projects/gitlab-automation/Config.py
@@ -0,0 +1,10 @@
+class Config:
+    url_gitlab = "https://gitlab.one.fanuc.com/"
+    url_gitlab = "https://localhost:42901"
+    gitlab_access_token = "d_pGddyowGuBGu4TsmMR"
+
+    Runtime = Runtime
+
+
+class Runtime:
+    Mode = True
